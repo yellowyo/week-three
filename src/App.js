ab711621a06c57efb29a52dc915e79277d46c16d
Alina Nedashkov <alinanedashkov@MacBook-Air.local>
2022-06-19 15:11:26 +0300

    firts commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+import { Component } from 'react';
+import { Heart } from './Heart';
+
+class App extends Component {
+  render() {
+      return <Heart/>
+  }
+}
+
+export default App;
---
+++ src/Heart.js
@@ -0,0 +1,9 @@
+import Component from 'react';
+
+export class Heart extends Component {
+    render() {
+        return <div>
+            <h1>Hi</h1>
+        </div>
+    }
+}
